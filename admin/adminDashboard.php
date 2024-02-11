742c3d0019bc7d8f3b99464611e12579847c0d44
srhhisam <107802977+srhhisam@users.noreply.github.com>
2024-02-12 01:13:09 +0800

    Update adminDashboard.php

--- admin/adminDashboard.php
+++ admin/adminDashboard.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Heather Dashboard</title>
+    <title>Heather</title>
+
     <style>
         body, html {
             height: 100%;
